144d299a01a4a55ea269061c2e76e44b36247dd4
ashah7 <akshat.shah3001@gmail.com>
2016-11-29 19:11:18 -0500

    Added Deployment scripts

---
+++ deploy/setup-env-vars.sh
@@ -0,0 +1,5 @@
+echo 'export TESTBOT_TOKEN="xoxb-91938846433-MjiuU4Jean6yMFr30vUrQLhG"' >> ~/.bashrc
+echo 'export DOCKERBOT_TOKEN="xoxb-94302876704-Zkw82KLMPHn91DeljqEyqFsL"' >> ~/.bashrc
+
+source /home/ubuntu/.bashrc
+source /home/ubuntu/.bashrc
